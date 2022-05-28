d283f9436d138ea6b7c3539759d342c83b73123c
Jim Shield <gothicjim@gmail.com>
2022-05-28 19:27:38 +0100

    Updated info + example of new locations

--- config.lua
+++ config.lua
@@ -1,15 +1,15 @@
 print("Jim-JobGarage v1.0 - Job Garage Script by Jimathy")
 
 Config = {
-	Debug = false,
-	CarDespawn = true,
+	Debug = false,  -- Enable to use debug features
+	CarDespawn = true, -- Disable to remove 
 	Locations = {
 		[1] = {
 			zoneEnable = true,
 			job = "mechanic",
 			garage = { 
-				spawn = vector4(-179.34, -1285.27, 30.83, 89.24),
-				out = vector4(-177.1, -1282.25, 31.3, 179.01),
+				spawn = vector4(-179.34, -1285.27, 30.83, 89.24),  -- Where the car will spawn
+				out = vector4(-177.1, -1282.25, 31.3, 179.01),	-- Where the parking stand is
 				list = {
 					["cheburek"] = {
 						--colors = { 136, 137 }, -- Color index numbers { primary, secondary },
@@ -25,5 +25,17 @@ Config = {
 				},
 			},
 		},
+		--[[[2] = {
+			zoneEnable = true,
+			job = "mechanic",
+			garage = { 
+				spawn = vector4(-179.34, -1285.27, 30.83, 89.24),
+				out = vector4(-177.1, -1282.25, 31.3, 179.01),
+				list = {
+					["cheburek"] = { },
+					["burrito3"] = { },
+				},
+			},
+		},]]
 	},
 }
