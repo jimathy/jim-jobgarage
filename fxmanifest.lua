425c7dcd699c4736fa3780edebc75033d01af369
Jim Shield <gothicjim@gmail.com>
2022-08-25 18:00:02 +0100

    Remove polyzone load/requirement from fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,15 +5,7 @@ description "Job Garage Script By Jimathy"
 fx_version "cerulean"
 game "gta5"
 
-client_scripts {
-    '@PolyZone/client.lua',
-    '@PolyZone/BoxZone.lua',
-    '@PolyZone/EntityZone.lua',
-    '@PolyZone/CircleZone.lua',
-    '@PolyZone/ComboZone.lua',
-	'client.lua',
-}
-
+client_scripts { 'client.lua', }
 server_scripts { 'server.lua', }
 shared_scripts { 'config.lua', 'functions.lua' }
 
